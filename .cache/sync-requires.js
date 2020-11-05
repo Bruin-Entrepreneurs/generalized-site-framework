7b3f8a1acd404a1ce323010db5693554ae1e56bc
Collin Prince <collin@Collins-MBP-2.lan>
2020-11-04 17:49:09 -0800

    build html output

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/collin/Desktop/code/be/generalized-site-framework/.cache/dev-404-page.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/collin/Desktop/code/be/generalized-site-framework/src/pages/index.js")))
 }
 
